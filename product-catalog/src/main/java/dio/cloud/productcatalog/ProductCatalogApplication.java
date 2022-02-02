e984c83e3b06ba520bad7a5f60a3ef4c38ef587c
nerm <nadir.rafagnim@gft.com>
2022-02-02 15:39:21 -0300

    Inicial

---
+++ product-catalog/src/main/java/dio/cloud/productcatalog/ProductCatalogApplication.java
@@ -0,0 +1,13 @@
+package dio.cloud.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogApplication.class, args);
+	}
+
+}
